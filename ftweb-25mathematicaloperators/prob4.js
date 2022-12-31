9e6398ff7135de602723aa219eb66877b87084aa
saurabh bhatt <saurabh7412@gmail.com>
2022-12-31 16:36:42 +0530

    add second day assignment

---
+++ ftweb-25mathematicaloperators/prob4.js
@@ -0,0 +1,16 @@
+
+let x= 500;
+
+let y= 500;
+if (x<y)
+{
+console.log("lesser");
+}
+else if(x=y)
+{
+  console.log("equal");
+}
+else
+ console.log( "greater");
+
+
---
+++ ftweb-25mathematicaloperators/problem1.js
@@ -0,0 +1,13 @@
+let a=3;
+let b=2;
+console.log("sum is", a+b);
+
+console.log("sub is", a-b);
+
+console.log("mult is", a*b);
+
+console.log("div is", a/b);
+
+console.log("mod is ",a%b);
+
+console.log(a**2);
